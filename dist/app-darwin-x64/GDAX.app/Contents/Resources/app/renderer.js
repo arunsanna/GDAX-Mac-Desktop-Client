eaacdda6a96df99cc526fdb5cdc5dfbea2e694e9
Ben Moore <benmoore422@gmail.com>
2017-12-12 02:38:22 +0100

    first commit

---
+++ dist/app-darwin-x64/GDAX.app/Contents/Resources/app/renderer.js
@@ -0,0 +1,5 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+<script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
+<script>if (window.module) module = window.module;</script>
